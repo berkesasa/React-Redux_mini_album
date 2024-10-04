2039ad6ff8b72c53a8cad54097df55760c2aca63
berkesasa <berkesasa11@gmail.com>
2024-10-04 20:46:23 +0300

    created userlist and skeleton animation

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {fetchUsers} from '../store'
+import Skeleton from './Skeleton'
 
 function UsersList() {
+  const {isLoading, data, error} = useSelector((state) => {
+    return state.users
+  })
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [dispatch]);
+
+if (isLoading) {
+  return <Skeleton times={10} className='h-10 w-full'/>
+}
+
+if(error){
+  return <div>Error fetching data...</div>
+}
+
   return (
-    <div className='text-2xl'>UsersList</div>
+    <div className='text-2xl'>
+      {data.length}
+    </div>
   )
 }
 
