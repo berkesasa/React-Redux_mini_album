0f6ac80eca440a95526018f2224195a3a2bda047
berkesasa <berkesasa11@gmail.com>
2024-10-07 22:51:45 +0300

    add user button added

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,32 +1,62 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {fetchUsers} from '../store'
-import Skeleton from './Skeleton'
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import Button from './Button';
+import Skeleton from './Skeleton';
 
 function UsersList() {
-  const {isLoading, data, error} = useSelector((state) => {
-    return state.users
-  })
 
-  const dispatch = useDispatch()
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false)
+  const [loadingUsersError, setLoadingUsersError] = useState(null)
+
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
 
   useEffect(() => {
+    setIsLoadingUsers(true)
     dispatch(fetchUsers())
+      .unwrap()
+      .catch((err) => {
+        setLoadingUsersError(err)
+      })
+      .finally(() => {
+        setIsLoadingUsers(false)
+      })
   }, [dispatch]);
 
-if (isLoading) {
-  return <Skeleton times={10} className='h-10 w-full'/>
-}
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
 
-if(error){
-  return <div>Error fetching data...</div>
-}
+  if (isLoadingUsers) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+
+  if (loadingUsersError) {
+    return <div>Error fetching data...</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
 
   return (
-    <div className='text-2xl'>
-      {data.length}
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
     </div>
-  )
+  );
 }
 
-export default UsersList
+export default UsersList;
