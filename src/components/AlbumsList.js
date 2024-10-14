17497a0964fdb5d4a25461fedc410f21268721ec
berkesasa <berkesasa11@gmail.com>
2024-10-14 17:56:02 +0300

    added rtk query and albumsApi

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useFetchAlbumsQuery } from '../store'
+import Skeleton from './Skeleton'
+import ExpandedPanel from './ExpandablePanel'
+import Button from './Button'
 
 function AlbumsList({ user }) {
+
+    const { data, error, isLoading } = useFetchAlbumsQuery(user)
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} />
+    } else if (error) {
+        content = <div>Error loading albums</div>
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>
+            return <ExpandedPanel key={album.id} header={header}>
+                List of photos in the album
+            </ExpandedPanel>
+        })
+    }
+
     return (
-        <div>AlbumsList {user.name}</div>
+        <div>
+            <div>AlbumsList {user.name}</div>
+            <div>{content}</div>
+        </div>
     )
 }
 
